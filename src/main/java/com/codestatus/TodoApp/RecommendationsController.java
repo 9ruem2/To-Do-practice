8936368400f23535ea6a706850c9611dc3bfdcef
구름 <118735475+9ruem2@users.noreply.github.com>
2023-04-12 16:19:36 +0900

    to-do-Application! 화면출력하기 test1

--- src/main/java/com/codestatus/TodoApp/RecommendationsController.java
+++ src/main/java/com/codestatus/TodoApp/RecommendationsController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class RecommendationsController {
     @GetMapping("/")
     public String helloworld(){
-        return "To-do Application!";
+        return "To-do Application !";
     }
 }
